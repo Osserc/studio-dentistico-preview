33a4bcbd6fef6dcccd2312d1f6da49362dccf6db
Stefano Rondelli <rstefanofil@hotmail.com>
2022-07-13 18:06:01 +0200

    Add animations on load and scroll, try making a fixed home button

---
+++ javascript/scripts.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", reveal);
+
+reveal();
+
+function reveal() {
+    var targets = document.querySelectorAll(".target");
+    for (var i = 0; i < targets.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = targets[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        targets[i].classList.add("active");
+      }
+    }
+}
